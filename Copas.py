2e460237df25f1f181aec8eae72b5fb1eb1b01d8
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-15 19:07:21 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -7,12 +7,12 @@ st.title("Copa do Mundo Futebol - FIFA")
 
 st.sidebar.header("País Sede")
 
-copas = pd.read_csv('https://github.com/manoelmr1984/copas_mundo_fifa_futebol/blob/main/WorldCups.csv')
-list_campeoes=[]
-list_campeoes=copas['Country'].values.tolist()
+#copas = pd.read_csv('https://github.com/manoelmr1984/copas_mundo_fifa_futebol/blob/main/WorldCups.csv')
+#list_campeoes=[]
+#list_campeoes=copas['Country'].values.tolist()
 
-selected_league = st.sidebar.selectbox('League',list_campeoes)
-#selected_league = st.sidebar.selectbox('League',['England','Germany','Italy','Spain','France'])
+#selected_league = st.sidebar.selectbox('League',list_campeoes)
+selected_league = st.sidebar.selectbox('League',['England','Germany','Italy','Spain','France'])
 
 st.sidebar.header("Ano Copa")
 selected_season = st.sidebar.selectbox('Season', ['2021/2022','2020/2021','2019/2020'])

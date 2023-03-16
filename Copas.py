a8a1c9a7382daef5fd79065f6da81ba210cd35d6
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-16 10:48:10 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -24,7 +24,7 @@ st.dataframe(df_copas)
 
 df_campeao = copas.loc[copas['Year_Country'] == selected_copa]
 #df_copas = load_copa(selected_copa)
-df_campeao = df_campeao.set_index('Winner')
+df_campeao = df_campeao.set_index('Year_Country')
 st.subheader("Copa de " + df_campeao['Winner'])
 #st.dataframe(df_campeao)
 

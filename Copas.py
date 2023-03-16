228bdcd3f4fb4dca06e441f2c6dd618f28508f3d
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-16 11:00:57 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -12,17 +12,6 @@ list_year_country = []
 list_year_country = copas['Year_Country'].values.tolist()
 selected_copa = st.sidebar.selectbox('',list_year_country)
 
-#def load_copa(year_country):
-#  year_country = selected_copa
-#  return copas.loc[copas['Year_Country'] == year_country]
-
-#df_copas = load_copa(selected_copa)
-#df_copas = df_copas.set_index('Year_Country')
-#st.subheader("Copa de " + selected_copa)
-#st.dataframe(df_copas)
-
-
-
 
 df_campeao = copas.loc[copas['Year_Country'] == selected_copa]
 #df_copas = load_copa(selected_copa)
@@ -33,24 +22,3 @@ st.dataframe(df_campeao)
 
 
 st.subheader("CAMPEÃO: " + df_campeao['Winner'])
-
-
-
-
-
-
-#list_campeoes = []
-#list_campeoes = df_copas['Winner'].values.tolist()
-#campeao = df_copas['Winner'].values
-#st.subheader(campeao)
-
-
-
-#def load_campeao(campeao): 
-#  campeao = selected_copa[0]
-#  return copas.loc[copas['Winner'] == campeao]
-
-#df_campeao = load_campeao(selected_copa)
-#df_campeao = df_campeao.set_index('Winner')
-#st.subheader("CAMPEÃO:  " + selected_copa)
-#st.dataframe(df_campeao)

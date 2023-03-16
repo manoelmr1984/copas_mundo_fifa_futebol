1a3a99fcb6ff937f14887d7711ddaf0b79a2026f
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-16 10:54:41 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -12,14 +12,14 @@ list_year_country = []
 list_year_country = copas['Year_Country'].values.tolist()
 selected_copa = st.sidebar.selectbox('',list_year_country)
 
-def load_copa(year_country):
-  year_country = selected_copa
-  return copas.loc[copas['Year_Country'] == year_country]
+#def load_copa(year_country):
+#  year_country = selected_copa
+#  return copas.loc[copas['Year_Country'] == year_country]
 
-df_copas = load_copa(selected_copa)
-df_copas = df_copas.set_index('Year_Country')
-st.subheader("Copa de " + selected_copa)
-st.dataframe(df_copas)
+#df_copas = load_copa(selected_copa)
+#df_copas = df_copas.set_index('Year_Country')
+#st.subheader("Copa de " + selected_copa)
+#st.dataframe(df_copas)
 
 
 df_campeao = copas.loc[copas['Year_Country'] == selected_copa]

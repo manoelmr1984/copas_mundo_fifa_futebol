eab058980297d5c912e85d42827dbc9cc7de54be
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-15 22:30:32 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -10,14 +10,13 @@ copas['Year_Country'] = copas['Year']+" - "+copas['Country']
 st.sidebar.header("Ano Copa")
 list_copa=[]
 list_copa=copas['Year_Country'].values.tolist()
-selected_season = st.sidebar.selectbox('Season',list_copa)
+selected_copa = st.sidebar.selectbox('Season',list_copa)
 
 def load_data(season):   
-  season = selected_season
-  #return copas
+  season = selected_copa
   return copas.loc[copas['Year_Country']==season]
 
-df = load_data(selected_season)
+df = load_data(selected_copa)
 df=df.set_index('Year_Country')
-st.subheader("Copa de "+selected_season)
+st.subheader("Copa de "+selected_copa)
 st.dataframe(df)

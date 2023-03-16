bc94c891929758faa9e79d6508a1daf81cb9c4bb
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-15 22:28:04 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-#import numpy as np
-#import base64
 
 st.title("Copa do Mundo Futebol - FIFA")
 
@@ -10,14 +8,14 @@ copas['Year'] = copas['Year'].astype(str)
 copas['Year_Country'] = copas['Year']+" - "+copas['Country']
 
 st.sidebar.header("Ano Copa")
-list_ano=[]
-list_ano=copas['Year'].values.tolist()
-selected_season = st.sidebar.selectbox('Season',list_ano)
+list_copa=[]
+list_copa=copas['Year_Country'].values.tolist()
+selected_season = st.sidebar.selectbox('Season',list_copa)
 
 def load_data(season):   
   season = selected_season
   #return copas
-  return copas.loc[copas['Year']==season]
+  return copas.loc[copas['Year_Country']==season]
 
 df = load_data(selected_season)
 df=df.set_index('Year_Country')

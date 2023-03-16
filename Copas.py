e4ea7ee864cb737a7212edce16864081248c66e9
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-16 09:43:38 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -22,6 +22,5 @@ st.subheader("Copa de "+selected_copa)
 st.dataframe(df_copas)
 
 list_campeoes = []
-#list_campeoes = copas['Winner'].values.tolist()
-list_campeoes = df_copas['Winner'].values.tolist()
+list_campeoes = df_copas['Winner']#.values.tolist()
 st.subheader(list_campeoes)

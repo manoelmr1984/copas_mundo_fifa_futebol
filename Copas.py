9bc7a55abfffec19f74c3f13ee04469b078621d1
manoelmr1984 <118855531+manoelmr1984@users.noreply.github.com>
2023-03-16 09:35:31 -0300

    Update Copas.py

--- Copas.py
+++ Copas.py
@@ -8,18 +8,19 @@ copas['Year'] = copas['Year'].astype(str)
 copas['Year_Country'] = copas['Year']+" - "+copas['Country']
 
 st.sidebar.header("Selecione a Copa")
-list_copa=[]
-list_copa=copas['Year_Country'].values.tolist()
+list_copa = []
+list_copa = copas['Year_Country'].values.tolist()
 selected_copa = st.sidebar.selectbox('',list_copa)
 
 def load_data(ano_anfitriao):   
   ano_anfitriao = selected_copa
-  return copas.loc[copas['Year_Country']==ano_anfitriao]
+  return copas.loc[copas['Year_Country'] == ano_anfitriao]
 
 df = load_data(selected_copa)
 df = df.set_index('Year_Country')
 st.subheader("Copa de "+selected_copa)
 st.dataframe(df)
 
-campeao = df['Year_Country'].values.tolist()
+list_campeoes = []
+list_campeoes = df['Year_Country'].values.tolist()
 st.subheader(camepao)
